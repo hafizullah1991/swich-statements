e16dc1da14be6e3a044db9e4d07794b15ddebfab
hafizullah1991 <Ahmadshakhi2020@gmail.com>
2024-11-08 21:34:53 -0800

    swich statement

---
+++ index.js
@@ -0,0 +1,23 @@
+let day = 3;
+switch(day){
+    case 1:
+        console.log("it's monday");
+        break;
+    case 2:
+        console.log("it's Tuesday");
+        break;
+    case 3:
+        console.log("it's wedensday");
+        break;
+    case 4:
+        console.log("thursday");
+        break;
+    case 5:
+        console.log("it's Friday");
+        break;
+    default:
+        console.log("it's not a day")
+    
+}
+
+let testScore = 92;
